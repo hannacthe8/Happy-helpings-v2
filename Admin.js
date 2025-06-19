a5c1e8e2e9286580a9107fb2211c8f0cc8deefb9
hannacthe8 <hannac@the8camerons.ca>
2025-06-19 15:53:52 -0400

    Add files via upload

---
+++ Admin.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Admin() {
+  return <div><h1>Admin Dashboard (Coming Soon)</h1></div>;
+}
---
+++ Specials.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Specials() {
+  return <div><h1>Specials</h1></div>;
+}
